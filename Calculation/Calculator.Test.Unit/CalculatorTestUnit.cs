c28599d3eb146073b8d95053117e32d24850cc47
CasperJI <201609470@post.au.dk>
2018-02-01 11:48:23 +0100

    Added Calculator

---
+++ Calculation/Calculator.Test.Unit/CalculatorTestUnit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    public class CalculatorTestUnit
+    {
+        [TestFixture]
+        public class CalculatorTest
+        {
+            [Test]
+            public void Add2And4_Returns6()
+            {
+                var uut = new Calculator();
+
+                Assert.That(uut.Add(2, 4), Is.EqualTo(6));
+            }
+
+
+        }
+    }
+}
