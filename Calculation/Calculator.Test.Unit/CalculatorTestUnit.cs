5f5d5a25d743d60dcda0644eb0ba9d2bea5ed2f8
CasperJI <201609470@post.au.dk>
2018-02-06 11:08:54 +0100

    Added som more tests to the unittesting

--- Calculation/Calculator.Test.Unit/CalculatorTestUnit.cs
+++ Calculation/Calculator.Test.Unit/CalculatorTestUnit.cs
@@ -19,6 +19,27 @@ namespace Calculator.Test.Unit
 
                 Assert.That(uut.Add(2, 4), Is.EqualTo(6));
             }
+            [Test]
+            public void Subtract2From4_Returns2()
+            {
+                var uut = new Calculator();
+
+                Assert.That(uut.Subtract(4, 2), Is.EqualTo(2));
+            }
+            [Test]
+            public void Multiply2With4_Returns8()
+            {
+                var uut = new Calculator();
+
+                Assert.That(uut.Multiply(2, 4), Is.EqualTo(8));
+            }
+            [Test]
+            public void TwoToThePowerOf4()
+            {
+                var uut = new Calculator();
+
+                Assert.That(uut.Power(2, 4), Is.EqualTo(16));
+            }
 
 
         }
