89e64edf0f715aa6c3083a1a7c4a272993b7cc25
CasperJI <201609470@post.au.dk>
2018-02-22 12:14:38 +0100

    it builded

--- RouletteGame/RouletteTest/IRoiulette.cs
+++ RouletteGame/RouletteTest/IRoiulette.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RouletteGame.Legacy;
 
 namespace Test_Roulette
 {
-    
+    public interface IRoulette
+    {
+        void spin();
+        Field GetResult();
+
+    }
 }
